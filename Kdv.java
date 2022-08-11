9e869e5ba335e32b2bb6f1d80cd1169e938f349d
KaanCihan <kaann.cihan@gmail.com>
2022-08-12 01:09:06 +0300

    kdv hesaplama

---
+++ Kdv.java
@@ -0,0 +1,40 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class Kdv {
+    public static void main(String[] args){
+
+        double kdvFiyat, kdvsizFiyat, kdv, fiyat;
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Fiyat tutarýný giriniz :");
+        fiyat = input.nextDouble();
+
+        boolean oran = fiyat <= 1000;
+        double kdvOrani;
+
+        kdvOrani = oran ? 0.18 : 0.19 ;
+
+        kdv = fiyat * kdvOrani;
+        kdvsizFiyat = fiyat - kdv;
+
+        System.out.println("KDV dahil fiyat : " + fiyat);
+        System.out.println("KDV dahil olmadan fiyat : " + kdvsizFiyat);
+        System.out.println("KDV orani : " + kdvOrani);
+        System.out.print("KDV fiyatý : " + kdv);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
