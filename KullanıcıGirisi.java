e45d1ab37a3076f6d1cf0102eb085333f08b5735
KaanCihan <kaann.cihan@gmail.com>
2022-08-15 00:36:31 +0300

    Kullanıcı Girişi programı eklendi

---
+++ KullanıcıGirisi.java
@@ -0,0 +1,38 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class KullanýcýGirisi {
+    public static void main (String [] args){
+        String userName, password, newPassword;
+
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Kullanýcý adýnýzý giriniz : ");
+        userName = input.nextLine();
+        System.out.print("Þifrenizi giriniz : ");
+        password = input.nextLine();
+
+        if (userName.equals("patika") && password.equals("java123")){
+
+            System.out.print("Baþarýyla giriþ yaptýnýz.");
+        }else if(! password.equals("java123") && userName.equals("patika")){
+            System.out.print("Þifreniz yanlýþ. Lütfen yeni þifre oluþturunuz : ");
+            newPassword = input.nextLine();
+            if (newPassword.equals("java123")){
+                System.out.print("Þifre oluþturulamadý, lütfen baþka þifre giriniz.");
+            }
+            else if (! newPassword.equals("java123")){
+                System.out.print("Yeni þifreniz baþarýyla oluþturulmuþtur.");
+            }
+        }
+        else{
+            System.out.print("Kullanýcý ismi bulunamadý.");
+        }
+
+
+
+
+    }
+}
