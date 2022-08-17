cf0fdbf94226a85a72761ca543ac857d81973133
KaanCihan <kaann.cihan@gmail.com>
2022-08-17 22:55:17 +0300

    JavaIfElse eklendi

---
+++ JavaIfElse.java
@@ -0,0 +1,28 @@
+package Giris;
+
+
+import java.util.Scanner;
+
+public class JavaIfElse {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        if(n % 2 == 1){
+            System.out.println("Weird");
+
+        }else{
+            if((2 <= n && n <= 5)  || (n > 20)) System.out.println("Not Weird");
+
+            if(6 <= n && n <= 20) System.out.println("Weird");
+        }
+
+
+
+
+
+
+
+    }
+}
