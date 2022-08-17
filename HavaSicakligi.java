0e4e3151dda8213a64aac687cb6665eca0591a7d
KaanCihan <kaann.cihan@gmail.com>
2022-08-17 21:08:59 +0300

    hava sicakligi eklendi

---
+++ HavaSicakligi.java
@@ -0,0 +1,45 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class HavaSicakligi {
+    public static void main(String args []){
+        int sicaklik;
+
+        Scanner input = new Scanner(System.in);
+        sicaklik = input.nextInt();
+
+        if(5 > sicaklik ){
+
+            System.out.print("Bu sýcaklýkta kayak yapmanýz önerilir.");
+
+        }
+        if(sicaklik == 5){
+
+            System.out.print("Bu sýcaklýkta kayak yapabilir veya sinemaya gidebilirsiniz.");
+        }
+        if(5 < sicaklik && sicaklik <15){
+
+            System.out.print("Bu sýcaklýkta sinemaya gidebilirsiniz.");
+        }
+        if(sicaklik == 15){
+
+            System.out.print("Bu sýcaklýkta piknik yapabilir veya sinemaya gidebilirsiniz.");
+        }
+        if(15 < sicaklik && sicaklik < 25){
+
+            System.out.print("Bu sýcaklýkta piknik yapabilirsiniz.");
+        }
+        if(sicaklik == 25){
+
+            System.out.print("Bu sýcaklýkta piknik yapabilir veya yüzmeye gidebilirsiniz.");
+        }
+        if(sicaklik > 25){
+
+            System.out.print("Bu sýcaklýkta yüzmeye gidebilirsiniz.");
+        }
+
+    }
+
+
+}
