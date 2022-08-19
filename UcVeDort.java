9ec9d54765eb60a0296b171908b7de384a528155
KaanCihan <kaann.cihan@gmail.com>
2022-08-19 04:22:02 +0300

    UcVeDort eklendi.

--- UcVeDort.java
+++ UcVeDort.java
@@ -2,21 +2,26 @@ package Giris;
 
 import java.util.Scanner;
 
-public class CiftSayi {
+public class UcVeDort {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int a;
+        int a, sum = 0, x = 0;
         System.out.print("Sayýyý giriniz : ");
         a = input.nextInt();
 
-        for (int i=0; i < a ; ++i){
+        for (int i=0; i < a ; ++i) {
 
-            if(i % 2 == 1){
+            if (!(i % 3 == 0) || !(i % 4 == 0)) {
+
                 continue;
             }
-            System.out.print( i + " ");
+            sum += i;
+            x++;
         }
+
+        System.out.println(sum / x);
+
 
 
 
