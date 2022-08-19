2238430440af5747ad1050edf711be1a6bccfdea
KaanCihan <kaann.cihan@gmail.com>
2022-08-19 05:11:59 +0300

    DortKatlari eklendi.

---
+++ DortKatlari.java
@@ -0,0 +1,23 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class DortKatlari {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        int total = 0;
+
+        do {
+            System.out.print("Sayý giriniz : ");
+            n = input.nextInt();
+
+            if(n % 4 == 0) {
+                total += n;
+            }
+        }while(n % 2 == 0);
+
+        System.out.println("toplam : " + total);
+
+    }
+}
