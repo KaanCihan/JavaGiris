f8eb278da227cacf3a2efd8ed04754a64a06eac9
KaanCihan <kaann.cihan@gmail.com>
2022-09-01 17:54:54 +0300

    REcursivePower eklendi.

---
+++ RecursivePower.java
@@ -0,0 +1,27 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class RecursivePower {
+
+    static int recursivePower(int taban, int us) {
+
+        if (us == 1)
+            return taban;
+
+        return recursivePower(taban, 1) * recursivePower(taban, us - 1);
+
+    }
+
+    public static void main(String[] args) {
+
+        int alt, ust;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Tabanı giriniz :");
+        alt = input.nextInt();
+        System.out.print("Üssü giriniz : ");
+        ust = input.nextInt();
+        System.out.print(recursivePower(alt, ust));
+
+    }
+}
