ef2ac41dff1d8b32b55f747f157d5367bab0cbd8
KaanCihan <kaann.cihan@gmail.com>
2022-09-03 04:03:47 +0300

    FizzBuzz eklendi.

---
+++ FizzBuzz.java
@@ -0,0 +1,21 @@
+package Giris;
+
+public class FizzBuzz {
+    public static void main(String args[]) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && !(i % 5 == 0)) {
+                System.out.print("fizz ");
+            } else if (i % 5 == 0 && !(i % 3 == 0))
+                System.out.print("buzz ");
+            else if (i % 3 == 0 && i % 5 == 0)
+                System.out.print("fizzbuzz ");
+            else {
+                System.out.print(i + " ");
+            }
+
+
+        }
+    }
+}
+
