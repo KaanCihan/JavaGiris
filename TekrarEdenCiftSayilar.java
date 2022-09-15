bcee975f1c0cfe5c8da76ce2ca9f2743e975d6c9
KaanCihan <kaann.cihan@gmail.com>
2022-09-15 14:34:55 +0300

    TekrarEdenCiftSAyilar eklendi.

--- TekrarEdenCiftSayilar.java
+++ TekrarEdenCiftSayilar.java
@@ -1,16 +1,17 @@
 import java.util.Arrays;
 
-public class TekrarEdenSayılar1 {
+public class TekrarEdenCiftSayilar {
+
     public static void main(String[] args) {
         int[] list = {1, 4, 2, 4, 78, 3, 3, 3, 3, 3, 7, 2, 89, 89, 78};
-        StringBuilder print = new StringBuilder("Tekrar eden sayılar : ");
+        StringBuilder print = new StringBuilder("Tekrar eden çift sayılar :");
         Arrays.sort(list);
         int tekrar = 0;
         int min = list[0];
         for (int j = 0; j < list.length; j++) {
 
             if (list[j] == min) {
-                if (!(tekrar == min)) {
+                if (!(tekrar == min) && (list[j] % 2 == 0)) {
                     tekrar = min;
                     print.append(" ").append(tekrar);
                 }
@@ -19,5 +20,7 @@ public class TekrarEdenSayılar1 {
         }
         System.out.println(print);
     }
-
 }
+
+
+
